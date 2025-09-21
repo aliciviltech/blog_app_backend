d78127360ee151cc94879ac1bce0bd872b9791f6
aliciviltech <ali.civiltech@gmail.com>
2025-09-21 22:27:31 +0500

    cors updated4

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ import { authRoutes } from './routes/authRoutes.js';
 dotenv.config()
 
 const app = express();
+app.use(cors())
 app.use(express.json({ limit: "50mb" }));  // Increase JSON limit
 app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase URL-encoded data limit
 // app.use(express.json());
-app.use(cors())
 
 // const allowedOrigins = [
 //   "http://localhost:5173",
