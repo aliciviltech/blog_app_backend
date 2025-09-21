17fc5585f1ac86e2c9eb986bb97cb5b8b949e4f6
aliciviltech <ali.civiltech@gmail.com>
2025-09-21 22:14:55 +0500

    cors updated2

--- index.js
+++ index.js
@@ -11,21 +11,26 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));  // Increase JSON limit
 app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase URL-encoded data limit
 // app.use(express.json());
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://baitulblog.vercel.app"
-];
-
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin:'http://localhost:5173',
   credentials: true,
-}));
+}))
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://baitulblog.vercel.app"
+// ];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// }));
 app.use(cookieParser())
 
 // routes 
@@ -44,5 +49,3 @@ const PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log('Server started:',PORT)
 })
-
-export default app;
